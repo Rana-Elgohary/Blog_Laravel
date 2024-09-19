001ba1b901377d6316dc5fceecfc72e9fd6100c9
Rana Elgohary <r.elgohary2000@gmail.com>
2024-09-19 03:10:00 +0300

    Layout

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,43 +1,19 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
+@section("title")
+Show
+@endsection
 
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Blog Post</a>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/post">All Posts</a>
-                    </li>
-                </ul>
-            </div>
+@section("content")
+<div class="container mt-3">
+    <div class="card">
+        <div class="card-header">
+            Post Info
         </div>
-    </nav>
-
-    <div class="container mt-3">
-        <div class="card">
-            <div class="card-header">
-              Post Info
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">Title: PHP</h5>
-              <p class="card-text">Description: With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-          </div>
+        <div class="card-body">
+            <h5 class="card-title">Title: PHP</h5>
+            <p class="card-text">Description: With supporting text below as a natural lead-in to additional content.</p>
+        </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+</div>
+@endsection
